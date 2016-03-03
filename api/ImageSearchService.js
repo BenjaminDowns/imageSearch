1ac88f50429424d96b011feaec0ff77ce3bc03fb
Benjamin Downs <benjamin.downs@gmail.com>
2016-03-03 19:09:05 +0100

    moving along

--- api/ImageSearchService.js
+++ api/ImageSearchService.js
@@ -1,4 +1,12 @@
+var db = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/imageSearch'
 
-module.exports = function (req, res) { 
+module.exports = function (req, res) {
     res.send("Here's your query: " + req.originalUrl.slice(17))
+    // var collection = db.collection['searches']
+
+    // collection.count(function (err, data) {
+    //     if (err) throw err
+    //     res.send("here's how many entries you have: " + data)
+    // })
+
 }
--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000
-var ImageSearchService = require('./api/ImageSearchService.js')
-var mongo = require('mongo')
+var mongoose = require('mongoose');
 var db = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/imageSearch'
+
+var searchHistory = new Schema({
+    search-terms: String,
+    time: Date;
+})
+
+var ImageSearchService = require('./api/ImageSearchService.js')
 
 mongo.connect(db, function (err, data) {
     if (err) throw err;
 
-    app.get('/api/imagesearch/*', ImageSearchService)
+    app.route('/api/imagesearch/*')
+        .get(ImageSearchService);
 
     app.get('/', function (req, res) {
         res.send('hello from index')
@@ -16,5 +23,4 @@ mongo.connect(db, function (err, data) {
 
     app.listen(port)
     console.log('listening on port ' + port)
-
 });
